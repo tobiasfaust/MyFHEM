852e3c28a2e6de75b4b1db36c9e45eaa73f1b617
tobiasfaust <tobias.faust@gmx.net>
2019-04-30 10:29:18 +0200

    add FHEM control file

--- hooks/pre-commit-hook.sh
+++ hooks/pre-commit-hook.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 cd ~/IdeaProjects/MyFHEM/
-controls_file="controls_echodevice.txt"
+controls_file="controls.txt"
 
-test -e "${controls_file}" && rm "${controls_file}"
+test -e $controls_file && rm $controls_file
 
 while IFS= read -r -d '' FILE
 do
@@ -11,7 +11,7 @@ do
     TIME=$(TZ=Europe/Berlin date -d "$TIME" +%Y-%m-%d_%H:%M:%S)
     FILESIZE=$(stat -c%s "$FILE")
 	FILE=$(echo "$FILE"  | cut -c 3-)
-	printf "UPD %s %-7d %s\n" "$TIME" "$FILESIZE" "$FILE"  >> controls.txt
+	printf "UPD %s %-7d %s\n" "$TIME" "$FILESIZE" "$FILE"  >> $controls_file
 done <   <(find ./FHEM -maxdepth 2 \( -name "*.pm" -o -name "*.txt" \) -print0 | sort -z -g)
 
 git add $controls_file
